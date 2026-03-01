96b0c119d431e278825b61aa93a7ed07ed988530
Greeshmanth04 <124453293+Greeshmanth04@users.noreply.github.com>
2026-03-01 12:14:32 +0530

    Delete DataVisualV2-main directory

--- DataVisualV2-main/backend/models/index.js
+++
@@ -1,3 +0,0 @@
-export { User } from './User.js';
-export { Dataset } from './Dataset.js';
-export { Dashboard } from './Dashboard.js';
--- DataVisualV2-main/backend/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as aiController from '../controllers/aiController.js';
-
-const router = express.Router();
-
-router.post('/generate-query', aiController.generateQuery);
-
-export default router;
